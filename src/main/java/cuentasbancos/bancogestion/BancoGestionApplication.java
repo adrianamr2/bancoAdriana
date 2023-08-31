4d2db5e5d96cd080cfd669d2f6484e0c4410cecc
amachacado <amachacado@conexia.com>
2023-08-31 16:47:07 -0500

    Initial commit

---
+++ src/main/java/cuentasbancos/bancogestion/BancoGestionApplication.java
@@ -0,0 +1,13 @@
+package cuentasbancos.bancogestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cuentasbancos/bancogestion/BancoGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package cuentasbancos.bancogestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
